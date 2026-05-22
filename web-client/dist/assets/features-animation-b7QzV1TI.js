bba1ecee1d6bc74fc72b1906004508b4536d92e0
hminh <hminh250104@gmail.com>
2026-05-22 20:35:48 +0700

    remove compiled dist directories

--- web-client/dist/assets/features-animation-b7QzV1TI.js
+++
@@ -1,1 +0,0 @@
-import{dQ as a,U as e,W as n}from"./index-260FghG0.js";const s={renderer:n,...e,...a};export{s as d};
--- web-client/dist/assets/src-UW24ZMRV-B1S915Qa.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./features-animation-b7QzV1TI.js";import"./index-260FghG0.js";var t=a;export{t as default};
